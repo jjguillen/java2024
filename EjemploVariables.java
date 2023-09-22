390f0bd2016ea882b12df613bb69144afbc9bfe7
jjguillen <jjavierguillen@gmail.com>
2023-09-22 18:00:45 +0200

    EjemploVariables

---
+++ EjemploVariables.java
@@ -0,0 +1,19 @@
+public class EjemploVariables {
+
+
+    public static void main(String[] args) {
+        
+        int numAlumnos;
+
+        numAlumnos = 17;
+
+        //Vienen dos alumnos al ciclo
+        numAlumnos = 19;
+
+        //En Noviembre se quitan 5
+        numAlumnos = 14;
+
+        System.out.println(numAlumnos);
+
+    }
+}
--- ejemplo.java
+++ ejemplo.java
@@ -2,7 +2,25 @@ class Ejemplo {
 
     public static void main(String[] args) {
 
-        System.out.println("Hola mundo");
+        int a,b,c;
+
+        b = 1;
+        c = 2;
+        a = b + c;
+
+        System.out.println("Hola mundo ");
+        System.out.println("Otro");
+
+
+        int c1 = 65;
+        int c2 = 43;
+
+
+        
+
+        System.out.println( c1 + c2);
+
+
 
     }
     
