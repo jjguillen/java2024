105cbec997cf5c9eb46d1a1461a410fdaaf4cb71
jjguillen <jjavierguillen@gmail.com>
2023-09-25 19:23:50 +0200

    hoy

---
+++ EjemploEnumerado.java
@@ -0,0 +1,11 @@
+public class EjemploEnumerado {
+    
+    public static void main(String[] args) {
+        
+        enum PosicionBasket { BASE, ALERO, ESCOLTA, ALAPIVOT, PIVOT }
+
+        System.out.println("Yo al basket juego de " + PosicionBasket.ALERO);
+
+        
+    }
+}
